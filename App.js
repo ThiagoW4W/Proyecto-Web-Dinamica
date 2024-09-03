1367a469d11334d6df44e20221e776246d0851e4
kiara osorio <kiaritaosorio2006@gmail.com>
2024-09-03 20:00:07 -0300

    subiendo login

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, extInput, Button, Alert, ImageBackground ,TouchableOpacity } from 'react-native';
+import Login from './login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Login/>
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center', 
   },
 });
